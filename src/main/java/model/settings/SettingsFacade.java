68cdb861f794b3526b421619c54d4e6f8a9ec0b5
louis roebben <louis.roebben@telenet.be>
2016-11-23 09:09:00 +0100

    def ship interface

---
+++ src/main/java/model/settings/SettingsFacade.java
@@ -0,0 +1,5 @@
+package model.settings;
+
+public class SettingsFacade
+{
+}
---
+++ src/main/java/model/type/ShipType.java
@@ -0,0 +1,11 @@
+package model.type;
+
+public enum ShipType
+{
+	Aircraftcarrier(5), BattleShip(4), Cruiser(3), Submarine(3), Destroyer(2)
+
+	ShipType(int Size)
+	{
+
+	}
+	}
