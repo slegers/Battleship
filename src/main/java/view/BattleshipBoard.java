6cb9e3c5fb834d93fbb5dd659db5d27a1e6d455a
KevinPeelmanR0485326 <kevin.peelman@student.ucll.be>
2016-11-23 08:45:49 +0100

    centre window

--- src/main/java/view/BattleshipBoard.java
+++ src/main/java/view/BattleshipBoard.java
@@ -3,6 +3,7 @@ package view;
 import controller.BattleshipController;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by yanice on 18/11/16.
@@ -16,10 +17,14 @@ public class BattleshipBoard extends JFrame{
     public BattleshipController getController(){
         return controller;
     }
+
     public void createBoard(){
         this.setTitle("Zeeslag");
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setSize(new Dimension(1000,500));
         this.setBounds(0,0,100,200);
         this.pack();
         this.setVisible(true);
+        this.setLocationRelativeTo(null);
     }
 }
