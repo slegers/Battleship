2a968edce71fc2f43a48bcf975fe6d5e6c11a872
KevinPeelmanR0485326 <kevin.peelman@student.ucll.be>
2016-11-23 08:58:08 +0100

    added fields

--- src/main/java/view/PlayerBoard.java
+++ src/main/java/view/PlayerBoard.java
@@ -1,12 +1,22 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.TreeMap;
 
 /**
  * Created by Kevin on 23/11/2016.
  */
 public class PlayerBoard extends JPanel {
-    ArrayList<Field> fields = new ArrayList<Field>();
+    TreeMap<Integer, Field> fields = new TreeMap<Integer, Field>();
 
+    public PlayerBoard(int fieldsize, int amountOfFields){
+        setLayout(new GridBagLayout());
+        for(int i = 0; i < amountOfFields; i++){
+            Field field = new Field();
+            fields.put(i,field);
+            add(field);
+        }
+    }
 }
