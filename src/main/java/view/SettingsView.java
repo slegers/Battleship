c3e50ea04ddefa15fe01cf7964025569b3008558
Slegers <yanice@wina.be>
2016-11-23 08:56:38 +0100

    SettingsView adapted

--- src/main/java/view/SettingsView.java
+++ src/main/java/view/SettingsView.java
@@ -9,6 +9,8 @@ import javax.swing.*;
  */
 public class SettingsView extends JFrame{
     private BattleshipController controller;
+    private JLabel speler1,speler2,width,height;
+    private JTextField speler1Text, speler2Text, widthText, heightText;
     public SettingsView(BattleshipController controller){
         this.controller = controller;
     }
@@ -16,9 +18,32 @@ public class SettingsView extends JFrame{
         return controller;
     }
     public void init(){
+        createComponents();
         setBounds(0,0,100,200);
         setTitle("Zeeslag - Instellingen");
         setVisible(true);
         pack();
     }
+
+    private void createComponents() {
+        speler1 = new JLabel();
+        add(speler1);
+        speler2 = new JLabel();
+        add(speler2);
+        width = new JLabel();
+        add(width);
+        height = new JLabel();
+        add(height);
+
+        speler1Text = new JTextField();
+        add(speler1Text);
+        speler2Text = new JTextField("Computer");
+        add(speler2Text);
+        widthText = new JTextField();
+        add(widthText);
+        heightText = new JTextField();
+        add(heightText);
+
+    }
+
 }
