aaf47de81dbbbbf32d5a3c361e35518f3d0aadde
Louis Roebben <louis.roebben@telenet.be>
2016-11-28 16:16:26 +0100

    get available ship count

--- src/main/java/model/type/ShipType.java
+++ src/main/java/model/type/ShipType.java
@@ -1,16 +1,7 @@
 package model.type;
 
-<<<<<<< HEAD
-public enum ShipType
-{
-	Aircraftcarrier(5), BattleShip(4), Cruiser(3), Submarine(3), Destroyer(2);
-
-	ShipType(int Size)
-	{
-=======
 public enum ShipType {
 	Aircraftcarrier(5,1), BattleShip(4,2), Cruiser(3,3), Submarine(3,3), Destroyer(2,4);
->>>>>>> 2f5672b69a40487a1d3ea718ab9de982c7ce3f2f
 
 	private int size;
 	private int maxShips;
