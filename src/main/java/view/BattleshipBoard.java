d0195595592334596f7de3ec00a0375e85dc47b5
KevinPeelmanR0485326 <kevin.peelman@student.ucll.be>
2016-11-28 19:08:01 +0100

    added combobox (example, not working yet)

--- src/main/java/view/BattleshipBoard.java
+++ src/main/java/view/BattleshipBoard.java
@@ -5,6 +5,8 @@ import model.settings.SettingsFacade;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by yanice on 18/11/16.
@@ -25,6 +27,19 @@ public class BattleshipBoard extends JFrame{
 
     public void createBoard(){
 
+        JPanel options = new JPanel();
+        options.setPreferredSize(new Dimension(300,15));
+        String[] ships = getController().getSettingsFacade().ge
+        String[] bookTitles = new String[] {"Effective Java", "Head First Java",
+                "Thinking in Java", "Java for Dummies"};
+        JComboBox<String> bookList = new JComboBox<>(bookTitles);
+        bookList.addActionListener (new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println((String) bookList.getSelectedItem());
+            }
+        });
+        options.add(bookList);
+
         JPanel player1Panel = new JPanel();
         player1Panel.setLayout(new FlowLayout());
         JLabel player1Label = new JLabel(getController().getSettingsFacade().getNamePlayer1());
@@ -43,13 +58,14 @@ public class BattleshipBoard extends JFrame{
         player2Panel.add(player2Label);
         player2Panel.add(player2);
 
-        setLayout(new GridLayout(1,2));
+        setLayout(new GridLayout(1,3));
+        add(options);
         add(player1Panel);
         add(player2Panel);
 
         this.setTitle("Zeeslag");
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setSize(new Dimension(700,380));
+        this.setSize(new Dimension(1050,380));
         //this.setBounds(0,0,100,200);
         //this.pack();
         this.setVisible(true);
