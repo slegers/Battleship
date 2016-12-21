4fd2fd3eeb815d5c35caea09f61a0700b347027a
Slegers <yanice@wina.be>
2016-12-21 18:48:57 +0100

    java observer broken, repaired

--- src/main/java/model/shipplacement/ShipPlacement.java
+++ src/main/java/model/shipplacement/ShipPlacement.java
@@ -112,6 +112,7 @@ public class ShipPlacement {
                         f.setColor(getHitColor());
                         target.setHit(true);
                         controller.getShipFacade("player").increaseSucesfullHits();
+
                         System.out.println("action");
                     } else if (target.getState().getClass().getSimpleName().equals("ForbiddenState")) {
                         if (f.getColor().equals(getStandardBackGroundColor())) {
@@ -126,6 +127,7 @@ public class ShipPlacement {
                     }
                 }
             }
+            controller.getShipFacade("player").notifyObservers();
         }
     }
 
