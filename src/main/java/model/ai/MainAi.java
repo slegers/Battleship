0c6234b0971afed4d6e7b616bc562067c840e2d2
louis roebben <louis.roebben@telenet.be>
2016-12-21 08:36:24 +0100

    AI uses settings

--- src/main/java/model/ai/MainAi.java
+++ src/main/java/model/ai/MainAi.java
@@ -1,13 +1,16 @@
 package model.ai;
 
 import controller.BattleshipController;
+import model.settings.SettingsFacade;
 
 /**
  * @author: Louis Roebben
  */
-class MainAi implements Action
+class MainAi implements Action, AiSettings
 {
 	private AiState aiState;
+	private SettingsFacade settingsFacade;
+
 
 	public MainAi() {
 		this.aiState = AiStateFactory.createPlaceState();
@@ -15,10 +18,16 @@ class MainAi implements Action
 	}
 
 	void placingDone() {
-		this.aiState = AiStateFactory.createAttackState();
+		this.aiState = AiStateFactory.createAttackState(settingsFacade);
 	}
 	@Override
 	public void doAction(BattleshipController battleshipController) {
 		aiState.getNextAction().doAction(battleshipController);
+	}
+
+	@Override
+	public void AiSettings(BattleshipController battleshipController)
+	{
+		this.settingsFacade = battleshipController.getSettingsFacade();
 	}
 }
